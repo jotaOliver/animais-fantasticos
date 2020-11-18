044a4ddfe7dba5191d3d8ad4c71f6d3cd76763c0
Jessé Oliveira <j.oliver.aguiar@gmail.com>
2020-11-18 20:29:50 -0300

    Mudado animação scroll de lugar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
+
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
@@ -13,7 +13,6 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js';
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
@@ -23,3 +22,6 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+initAnimacaoScroll();
